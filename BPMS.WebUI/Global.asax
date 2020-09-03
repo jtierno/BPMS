eb170aa095e1974c91b7b022e6a821cc1d524827
jtierno <john.tierno@outlook.com>
2020-09-03 18:27:57 -0400

    Add project files.

---
+++ BPMS.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BPMS.WebUI.MvcApplication" Language="C#" %>
---
+++ BPMS.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BPMS.WebUI.Startup))]
+namespace BPMS.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
